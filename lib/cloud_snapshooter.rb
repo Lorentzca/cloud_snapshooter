89587fab486fd9e2a6318d2276af130be1099ca5
Kota Kunishima <kunishima@feedforce.jp>
2014-12-10 21:18:13 +0900

    Add define of teke aws snapshot

--- lib/cloud_snapshooter.rb
+++ lib/cloud_snapshooter.rb
@@ -1,5 +1,14 @@
 require "cloud_snapshooter/version"
+require "aws-sdk"
+
+AWS.config(
+  :access_key_id => ENV['AWS_ACCESS_KEY'],
+  :secret_access_key => ENV['AWS_SECRET_KEY'],
+  :region => ENV['AWS_REGION']
+)
 
 module CloudSnapshooter
-  # Your code goes here...
+  def self.aws_snapshot(volume_id, description)
+    AWS.ec2.volumes[volume_id].create_snapshot(description)
+  end
 end
