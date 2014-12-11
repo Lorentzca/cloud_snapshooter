b5050796d9b134d57eb63bb8c9c2e24f86801b78
lorentzca <knsmkt2943@gmail.com>
2014-12-12 00:01:15 +0900

    Add initial spec

---
+++ spec/cloud_snapshooter_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe CloudSnapshooter do
+  it 'has a version number' do
+    expect(CloudSnapshooter::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
