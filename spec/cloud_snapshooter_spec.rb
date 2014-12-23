f2724fd9ab009155f3b82e6506868ad5171bbda1
Kota Kunishima <kunishima@feedforce.jp>
2014-12-23 20:53:50 +0900

    Version bump 1.0.3

--- spec/cloud_snapshooter_spec.rb
+++ spec/cloud_snapshooter_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe CloudSnapshooter do
   it 'has a version number' do
-    expect(CloudSnapshooter::VERSION).to eq('1.0.1')
+    expect(CloudSnapshooter::VERSION).to eq('1.0.3')
   end
 end
