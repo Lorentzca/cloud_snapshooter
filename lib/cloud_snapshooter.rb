56422653c47eda67f96e2a80a2341d714becb219
Kota Kunishima <kunishima@feedforce.jp>
2014-12-23 19:34:01 +0900

    Add Shoot class

--- lib/cloud_snapshooter.rb
+++ lib/cloud_snapshooter.rb
@@ -1,14 +1,5 @@
 require "cloud_snapshooter/version"
-require "aws-sdk"
-
-AWS.config(
-  :access_key_id => ENV['AWS_ACCESS_KEY'],
-  :secret_access_key => ENV['AWS_SECRET_KEY'],
-  :region => ENV['AWS_REGION']
-)
+require "cloud_snapshooter/shoot"
 
 module CloudSnapshooter
-  def self.ec2_snapshot(volume_id, description)
-    AWS.ec2.volumes[volume_id].create_snapshot(description)
-  end
 end
